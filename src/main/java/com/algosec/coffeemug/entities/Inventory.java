13bf1b5a921bbb7cdfa821f2939bcdc610d2b3b0
Kobi Ianko <kobi.ianko@algosec.com>
2022-11-08 17:43:37 +0200

    9th commit

--- src/main/java/com/algosec/coffeemug/entities/Inventory.java
+++ src/main/java/com/algosec/coffeemug/entities/Inventory.java
@@ -1,6 +1,8 @@
 package com.algosec.coffeemug.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,6 +14,8 @@ import java.util.UUID;
 
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "INVENTORY")
 public class Inventory {
 
--- src/test/java/com/algosec/coffeemug/Testing101_Mockito_03_InjectMocksTestWithSpringBoot.java
+++ src/test/java/com/algosec/coffeemug/Testing101_Mockito_03_InjectMocksTestWithSpringBoot.java
@@ -1,83 +1,58 @@
-//package com.algosec.coffeemug;
-//
-//import com.algosec.coffeemug.constants.DrinkType;
-//import com.algosec.coffeemug.constants.Ingredients;
-//import com.algosec.coffeemug.entities.Mug;
-//import com.algosec.coffeemug.repository.MugRepository;
-//import com.algosec.coffeemug.services.InventoryService;
-//import com.algosec.coffeemug.services.MugService;
-//import com.algosec.coffeemug.services.OrderService;
-//import javassist.expr.NewArray;
-//import lombok.extern.slf4j.Slf4j;
-//import org.hamcrest.CoreMatchers;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.junit.runner.RunWith;
-//import org.mockito.ArgumentMatchers;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.mockito.Spy;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.context.TestConfiguration;
-//import org.springframework.boot.test.mock.mockito.SpyBean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.TestPropertySource;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-//
-//import java.util.Date;
-//import java.util.UUID;
-//
-//@Slf4j
-//@RunWith(SpringRunner.class)
-//@ExtendWith(MockitoExtension.class)
-//@SpringBootTest(
-//    classes = CoffeemugApplication.class)
-//@TestPropertySource(
-//    locations = "classpath:application-test.properties")
-//class Testing101_Mockito_03_InjectMocksTestWithSpringBoot {
-//
-//    
-//    @InjectMocks
-//    private MugService mugService;
-//
-//    @Mock
-//    private MugRepository mugRepository;
-////    @SpyBean
-////    @Autowired
-////    private InventoryService inventoryService;
-//    @Mock
-//    private OrderService orderService;
-//
-//    @Test
-//    public void givenOneCoffeeMug_thenStatus200AndMilkIsOneLiter()
-//        throws Exception {
-//
-//        Mockito.doNothing().when(orderService).placeOrder(Ingredients.COFFE, 1);
-//        Mockito.doNothing().when(orderService).placeOrder(Ingredients.MILK, 1);
-//        
-//        Mug m = new Mug(UUID.randomUUID(), DrinkType.AMERICANO, new Date());
-//        Mockito.when(mugRepository.save(ArgumentMatchers.any())).thenReturn(m);
-////        inventoryService.getInventory();
-////        inventoryService.reduceInventory(drinkType.getCoffeeInGrams(), drinkType.getMilkInMilliliters());
-//        
-//        
-//        mugService.getDrink(DrinkType.AMERICANO);
-//            
-//    }
-//
-//
-//    @TestConfiguration
-//    @ComponentScan(basePackages = {"com.algosec.coffeemug.***"})
-//    static class Configuration {
-//
-//    }
-//	
-//}
+package com.algosec.coffeemug;
+
+import com.algosec.coffeemug.constants.DrinkType;
+import com.algosec.coffeemug.entities.Inventory;
+import com.algosec.coffeemug.entities.Mug;
+import com.algosec.coffeemug.repository.MugRepository;
+import com.algosec.coffeemug.services.InventoryService;
+import com.algosec.coffeemug.services.MugService;
+import com.algosec.coffeemug.services.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Slf4j
+@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension. class)
+class Testing101_Mockito_03_InjectMocksTestWithSpringBoot {
+
+    
+    @InjectMocks
+    private MugService mugService;
+    
+    @Mock
+    private MugRepository mugRepository;
+
+    @Mock
+    private InventoryService inventoryService;
+    @Mock
+    private OrderService orderService;
+
+    @Test
+    public void injectMocksTest()
+        throws Exception {
+
+        Mug m = new Mug(UUID.randomUUID(), DrinkType.AMERICANO, new Date());
+        Mockito.when(mugRepository.save(ArgumentMatchers.any())).thenReturn(m);
+
+        Inventory inventory = new Inventory(UUID.randomUUID(), new Date(), 1f, 1f);
+        Mockito.when(inventoryService.getInventory()).thenReturn(inventory);
+
+        Mockito.doNothing().when(inventoryService)
+        .reduceInventory(ArgumentMatchers.anyFloat(), ArgumentMatchers.anyFloat());
+        
+        mugService.getDrink(DrinkType.AMERICANO);
+            
+    }
+	
+}
